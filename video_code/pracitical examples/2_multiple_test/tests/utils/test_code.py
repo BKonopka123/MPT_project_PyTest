bdc20fb0fb199789c4940febad7a31c861610802
BKonopka123 <bkonopka@student.agh.edu.pl>
2023-11-29 22:26:29 +0100

    presentation code 29.11 2

--- video_code/pracitical examples/2_multiple_test/tests/utils/test_code.py
+++ video_code/pracitical examples/2_multiple_test/tests/utils/test_code.py
@@ -21,3 +21,46 @@ class Test_fun_1:
     def test_reverse(self):
         list1=[1,2,3]
         assert code.arr_reverse(list1) == [3,2,1]
+
+#pytest --markers
+@pytest.mark.arr
+def test_sort():
+    list1 = [3,2,1]
+    assert code.arr_sort(list1) == [1,2,3]
+
+@pytest.mark.arr
+def test_find():
+    list1 = [8,9,15]
+    assert code.arr_find(list1, 9) == 1
+
+@pytest.mark.arr
+def test_min():
+    list1 = [19,-5,16]
+    assert code.arr_min(list1) == -5
+
+@pytest.mark.arr
+def test_max():
+    list1 = [100,9,28]
+    assert code.arr_max(list1) == 100
+
+@pytest.mark.arr
+def test_reverse():
+    list1=[1,2,3]
+    assert code.arr_reverse(list1) == [3,2,1]
+
+
+@pytest.mark.math
+def test_sum():
+    assert code.sum(1,2) == 3
+
+@pytest.mark.math
+def test_diff():
+    assert code.diff(1,2) == -1
+
+@pytest.mark.math
+def test_multi():
+    assert code.multi(1,2) == 2
+
+@pytest.mark.math
+def test_div():
+    assert code.div(1,2) == 0.5
--- video_code/pracitical examples/2_multiple_test/utils/code.py
+++ video_code/pracitical examples/2_multiple_test/utils/code.py
@@ -15,3 +15,14 @@ def arr_reverse(arr):
     new_arr = arr[::-1]
     return new_arr
 
+def sum(a, b):
+    return a+b
+
+def diff(a, b):
+    return a-b
+
+def multi(a, b):
+    return a*b
+
+def div(a, b):
+    return a/b
