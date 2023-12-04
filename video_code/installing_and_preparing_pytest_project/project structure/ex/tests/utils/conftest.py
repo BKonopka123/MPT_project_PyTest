4c5b8002b68fc2906cf6f418d55e079894c8344e
BKonopka123 <bkonopka@student.agh.edu.pl>
2023-12-04 21:28:30 +0100

    preparing - structure update

--- video_code/installing_and_preparing_pytest_project/project structure/ex/tests/utils/conftest.py
+++ video_code/installing_and_preparing_pytest_project/project structure/ex/tests/utils/conftest.py
@@ -1,9 +1,1 @@
 from tests.utils import test_example
-
-
-def pytest_assertrepr_compare(op, left, right):
-    if isinstance(left, test_example.Foo) and isinstance(right, test_example.Foo) and op == "==":
-        return [
-            "Comparing Foo instances:",
-            f"   vals: {left.val} != {right.val}",
-        ]
--- video_code/installing_and_preparing_pytest_project/project structure/ex/tests/utils/test_example.py
+++ video_code/installing_and_preparing_pytest_project/project structure/ex/tests/utils/test_example.py
@@ -2,5 +2,5 @@ import pytest
 from utils import example
 
 
-def test_fun2():
-    example.fun2([1,2,3], (1,3,5), [3,2,1]) == [1,3] 
+def test_fun():
+    example.fun() == 1 
